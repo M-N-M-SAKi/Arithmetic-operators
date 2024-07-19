59db7c5181074eb990e16e21aec1f142df7cbf82
MH-ZaKy <168363944+M-N-M-SAKi@users.noreply.github.com>
2024-07-19 08:30:59 -0700

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,22 @@
+#take the first number as input
+input1 = input("Enter the first number: ")
+
+#take the second number as input
+input2 = input("Enter the second number: ")
+
+first_num = int(input1)
+second_num = int(input2)
+
+the_sum = first_num + second_num
+the_sb = first_num - second_num
+the_ml = first_num * second_num
+the_dv = first_num / second_num
+the_mo = first_num % second_num
+
+#print 
+print("Sum:",the_sum)
+print("Subtraction:",the_sb)
+print("Multiplication:",the_ml)
+print("Devision:",the_dv)
+print("Modulo:",the_mo)
+5
